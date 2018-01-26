e9d7d02e9abc6d041bc7403b909dc2e3c2e699a9
William Patton <will@pattonwebz.com>
2018-01-26 19:50:35 +0000

    PHPCS pass

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
- /**
-  * The functions.php file of the 'Seafoam' child theme of 'Best Reloaded' theme.
-  *
-  * @package Best Reloaded
-  * @subpackage Seafoam
-  */
+/**
+ * The functions.php file of the 'Seafoam' child theme of 'Best Reloaded' theme.
+ *
+ * @package Best Reloaded
+ * @subpackage Seafoam
+ */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -15,17 +15,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Enqueue this themes and set parent theme styles as dependancies.
  */
 function seafoam_enqueue_css() {
-	wp_enqueue_style( 'seafoam', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'best-reloaded', 'font-awesome' ) );
+	wp_enqueue_style( 'seafoam', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'best-reloaded' ) );
 }
 add_action( 'wp_enqueue_scripts', 'seafoam_enqueue_css' );
 
 /**
- * Filters the upsell values in the customizer.
+ * Filters the help and support message values in the customizer.
  *
  * This is a strait override, not truly a filter.
  *
  * @param  array $upsell_values An array of existing upsell values.
- *
  * @return array                Updated array of upsell values.
  */
 function seafoam_filter_upsell_values( $upsell_values ) {
@@ -40,11 +39,11 @@ function seafoam_filter_upsell_values( $upsell_values ) {
 	$description = ob_get_clean();
 	// start a holder array.
 	$upsell_values = array(
-		'title'    => esc_html__( 'Seafoam', 'seafoam' ),
-		'pro_text' => esc_html__( 'Help and Support',         'seafoam' ),
-		'pro_url'  => esc_url( 'https://www.pattonwebz.com/contact-me/' ),
+		'title'           => esc_html__( 'Seafoam', 'seafoam' ),
+		'pro_text'        => esc_html__( 'Help and Support', 'seafoam' ),
+		'pro_url'         => esc_url( 'https://www.pattonwebz.com/contact-me/' ),
 		'pro_description' => $description,
-		'priority'   => 10,
+		'priority'        => 10,
 	);
 	return $upsell_values;
 }
@@ -55,7 +54,6 @@ add_filter( 'best_reloaded_filter_upsell_values', 'seafoam_filter_upsell_values'
  * Filters the title of the main customizer panel to match our theme name.
  *
  * @param  string $title Current panel title.
- *
  * @return string        Updated panel title.
  */
 function seafoam_filter_theme_settings_panel_title( $title ) {
@@ -68,7 +66,6 @@ add_filter( 'best_reloaded_filter_theme_settings_panel_title', 'seafoam_filter_t
  * Filter the default values that are passed to the customizer object.
  *
  * @param array $defaults An array of mixed type key=>value data.
- *
  * @return array          updated array of values
  */
 function seafoam_filter_setting_defaults( $defaults ) {
@@ -87,7 +84,7 @@ function seafoam_setup() {
 	// Set the default background color for use in the theme. This overrules
 	// the same support added in the parent theme.
 	$custom_bg_args = array(
-			'default-color' => '1558a5',
+		'default-color' => '1558a5',
 	);
 	add_theme_support( 'custom-background', $custom_bg_args );
 }
